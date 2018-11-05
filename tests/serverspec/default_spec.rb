87e4563a0698e4ff16ccde0f4757afd7adb99cdd
Tomoyuki Sakurai <y@trombik.org>
2018-11-05 13:47:57 +0900

    feature: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,46 +3,33 @@ require "serverspec"
 
 package = "node_exporter"
 service = "node_exporter"
-config  = "/etc/node_exporter/node_exporter.conf"
 user    = "node_exporter"
 group   = "node_exporter"
-ports   = [PORTS]
-log_dir = "/var/log/node_exporter"
-db_dir  = "/var/lib/node_exporter"
+ports   = [9100]
+default_user = "root"
+default_group = "root"
+flags = ""
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/node_exporter.conf"
-  db_dir = "/var/db/node_exporter"
+  default_group = "wheel"
+  flags = "node_exporter_args='--log.format=\"logger:stderr\"'"
 end
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("node_exporter") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
 case os[:family]
 when "freebsd"
   describe file("/etc/rc.conf.d/node_exporter") do
+    it { should exist }
     it { should be_file }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    it { should be_mode 644 }
+    its(:content) { should match(/^# Managed by ansible$/) }
+    its(:content) { should match(/^#{flags}$/) }
   end
 end
 
