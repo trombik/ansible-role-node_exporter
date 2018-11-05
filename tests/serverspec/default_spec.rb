41fd57955378a4cea911bb0b15d1ab7fd6037b96
Tomoyuki Sakurai <240118+trombik@users.noreply.github.com>
2018-11-05 12:18:33 +0700

    feature: initial implementation (#1)
    * feature: initial implementation
    
    * feature; support Ubuntu
    
    * bugfix: QA
    
    * bugfix: test the output
    
    * bugfix: remove unused variables

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,48 +1,35 @@
 require "spec_helper"
 require "serverspec"
 
-package = "node_exporter"
-service = "node_exporter"
-config  = "/etc/node_exporter/node_exporter.conf"
-user    = "node_exporter"
-group   = "node_exporter"
-ports   = [PORTS]
-log_dir = "/var/log/node_exporter"
-db_dir  = "/var/lib/node_exporter"
+package = "prometheus-node-exporter"
+service = "prometheus-node-exporter"
+ports   = [9100]
+default_user = "root"
+default_group = "root"
+flags = ""
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/node_exporter.conf"
-  db_dir = "/var/db/node_exporter"
+  default_group = "wheel"
+  package = "node_exporter"
+  service = "node_exporter"
+  flags = "node_exporter_args='--log.format=\"logger:stderr\"'"
 end
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("node_exporter") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
 case os[:family]
 when "freebsd"
   describe file("/etc/rc.conf.d/node_exporter") do
+    it { should exist }
     it { should be_file }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    it { should be_mode 644 }
+    its(:content) { should match(/^# Managed by ansible$/) }
+    its(:content) { should match(/^#{flags}$/) }
   end
 end
 
@@ -55,4 +42,10 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe command "curl -s http://localhost:9100/metrics" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^node_load1\s+\d+\.\d+$/) }
 end
